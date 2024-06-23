10fc7a881739af9150393a1849ed8e28bfb7a38d
richardxnathan <richardnathand@gmail.com>
2024-06-22 22:09:12 -0300

    Implementation

---
+++ src/IPhone.java
@@ -0,0 +1,48 @@
+public class IPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet{
+
+    @Override
+    public void ligar() {
+
+    }
+
+    @Override
+    public void atender() {
+
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+
+    }
+
+    @Override
+    public void exibirPagina() {
+
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+
+    }
+
+    @Override
+    public void atualizarPagina() {
+
+    }
+
+    @Override
+    public void tocar() {
+
+    }
+
+    @Override
+    public void pausar() {
+
+    }
+
+    @Override
+    public void selecionarMusica() {
+
+    }
+
+}
